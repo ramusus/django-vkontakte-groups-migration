bc44ed5bb1c9ddfed4891f1da65b547e7df18c78
ramusus <ramusus@gmail.com>
2013-05-08 14:17:58 +0400

    bump version, updated factories 2.0

--- vkontakte_groups_migration/factories.py
+++ vkontakte_groups_migration/factories.py
@@ -2,7 +2,7 @@ from vkontakte_groups.factories import GroupFactory
 from models import GroupMigration
 import factory
 
-class GroupMigrationFactory(factory.Factory):
+class GroupMigrationFactory(factory.DjangoModelFactory):
     FACTORY_FOR = GroupMigration
 
     group = factory.SubFactory(GroupFactory)
