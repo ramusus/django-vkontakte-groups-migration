75d2b7ed9de4cc96f7a161e78d2a6d0e8f331530
ramusus <ramusus@gmail.com>
2014-04-05 13:35:18 +0600

    disable saving migration formset in admin

--- vkontakte_groups_migration/admin.py
+++ vkontakte_groups_migration/admin.py
@@ -1,11 +1,19 @@
 # -*- coding: utf-8 -*-
 from django.contrib import admin
+from django.forms.models import BaseInlineFormSet
 from vkontakte_api.admin import VkontakteModelAdmin
 from vkontakte_groups.admin import Group, GroupAdmin as GroupAdminOriginal
 from models import GroupMigration
 
+class GroupMigrationFormSet(BaseInlineFormSet):
+    def save_new(self, form, commit=True):
+        return
+    def save_existing(self, form, instance, commit=True):
+        return
+
 class GroupMigrationInline(admin.TabularInline):
     model = GroupMigration
+    formset = GroupMigrationFormSet
     fields = ('id','group','time','offset','hidden','members_count','members_entered_count','members_left_count')
     readonly_fields = fields
     ordering = ('-time',)
